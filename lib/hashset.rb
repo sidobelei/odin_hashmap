27ee40bea96d13f8162bb3cdef7e57f0ecaa7ee9
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-07-03 14:52:19 -0700

    Adapt set method from HashMap for use in HashSet

--- lib/hashset.rb
+++ lib/hashset.rb
@@ -16,4 +16,24 @@ class HashSet
         
     hash_code
   end
+
+  def set(key)
+    if has?(key)
+      return
+    end
+    if length + 1 > (@capacity * @load_factor).round
+      old_entries = entries
+      @length = 0
+      @capacity = @capacity * 2
+      clear
+      old_entries.each {|node| set(node[0])}
+    end 
+    bucket = hash(key) % @capacity
+    if @hashmap[bucket].nil?
+      @hashmap[bucket] = [key]
+    else
+      @hashmap[bucket].push(key)
+    end
+    @length += 1
+  end
 end
