f6b7c5c5fb2dac1ad0d0718b7744ffe5bab0e6c0
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-07-03 10:03:17 -0700

    Add initialize method to HashMap class

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,4 +1,13 @@
 class HashMap
+  attr_reader :length
+  
+  def initialize
+    @capacity = 16
+    @load_factor = 0.75
+    @length = 0
+    @hashmap = Array.new(@capacity)
+  end
+
   def hash(key)
     hash_code = 0
     prime_number = 31
@@ -6,5 +15,5 @@ class HashMap
     key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
         
     hash_code
-  end     
+  end
 end
